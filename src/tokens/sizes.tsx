b81d946071f2ce30c2ac814f462b7f33237bdf91
Elizabeth <elizabethmvm@gmail.com>
2025-02-22 11:06:13 -0500

    add token and styling settings

---
+++ src/tokens/sizes.tsx
@@ -0,0 +1,38 @@
+const Sizes = {
+    // Spacing
+    spacing: {
+        'none': '0rem',
+        '1': '0.0625rem',
+        '2': '0.125rem',
+        '4': '0.25rem',
+        '8': '0.5rem',
+        '16': '1rem',
+        '32': '2rem',
+    },
+    radius: {
+        'none': '0rem',
+        '4px': '0.25rem',
+        '8px': '0.5rem',
+        '16px': '1rem',
+        '32px': '2rem',
+    },
+    // text
+    text: {
+        '12': '0.75rem',
+        '16': '1rem',
+        '18': '1.125rem',
+        '23': '1.4375rem',
+        '29': '1.8215rem',
+        '36': '2.25rem',
+        '45': '2.8125rem',
+        '56': '3.5rem',
+        '70': '4.375rem',
+        '88': '5.5rem',
+    },
+    sem: {
+        'content-padding': '3rem',
+        'content-margin': '',
+        'navbar-height': '3rem',
+    },
+}
+export default Sizes
