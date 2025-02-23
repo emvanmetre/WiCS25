83c8732b6748abb597a27bd58cb0f319ee1286c9
Elizabeth <elizabethmvm@gmail.com>
2025-02-23 10:45:22 -0500

    try safely display html

--- src/components/componentview.tsx
+++ src/components/componentview.tsx
@@ -3,6 +3,8 @@ import { useMediaQuery } from 'react-responsive'
 import { Button, ButtonRound, Icon, Text } from './index'
 import { TextField, TextArea } from 'react-aria-components'
 import '../style.css'
+import DOMPurify from 'dompurify'
+import parse from 'html-react-parser'
 
 type ComponentViewProps = {
   id?: string
@@ -12,6 +14,12 @@ const ComponentView = (props: ComponentViewProps) => {
   const [bgDark, setBgDark] = useState(false)
   const [sizeLarge, setSizeLarge] = useState(false)
   const [tab, setTab] = useState('html')
+  const [curTabText, setCurTabText] = useState('')
+  const [htmlDisplay, setHtmlDisplay] = useState('')
+
+  const [htmlText, setHtmlText] = useState('')
+  const [cssText, setCssText] = useState('')
+  const [jsText, setJsText] = useState('')
 
   const handleBg = (light: boolean) => {
     if (light) {
@@ -29,27 +37,65 @@ const ComponentView = (props: ComponentViewProps) => {
     }
   }
 
+  const changeTab = (newTab: string) => {
+    setTab(newTab)
+    if (newTab == 'html') {
+      setCurTabText(htmlText)
+    } else if (newTab == 'css') {
+      setCurTabText(cssText)
+    } else {
+      setCurTabText(jsText)
+    }
+  }
+
+  function SafeHTMLDisplay({ htmlString }) {
+    const sanitizedHTML = DOMPurify.sanitize(htmlString, { KEEP_CONTENT: true })
+    console.log(sanitizedHTML)
+    return <div dangerouslySetInnerHTML={{ __html: sanitizedHTML }} />
+  }
+
+  const updateText = (text: string) => {
+    if (tab == 'html') {
+      setHtmlText(text)
+    } else if (tab == 'css') {
+      setCssText(text)
+    } else {
+      setJsText(text)
+    }
+    setCurTabText(text)
+    setHtmlDisplay(SafeHTMLDisplay(parse(text)))
+    //console.log(SafeHTMLDisplay(parse(text)))
+  }
+
   const isScreenSmall = useMediaQuery({ maxWidth: '1150px' })
   return (
     <>
       <div className="content col-100">
         <div className={`component-view ${bgDark ? 'comp-view-dark' : 'comp-view-light'}`}>
-          <ButtonRound className={`bg-white ${bgDark ? '' : 'selected'}`} size="md" onPress={() => handleBg(true)}></ButtonRound>
-          <ButtonRound className={`bg-dark ${bgDark ? 'selected' : ''}`} size="md" onPress={() => handleBg(false)}></ButtonRound>
+          {htmlDisplay}
+          <div className="button-row">
+            <ButtonRound className={`bg-white ${bgDark ? '' : 'selected'}`} size="md" onPress={() => handleBg(true)}></ButtonRound>
+            <ButtonRound className={`bg-dark ${bgDark ? 'selected' : ''}`} size="md" onPress={() => handleBg(false)}></ButtonRound>
+          </div>
         </div>
         <div className={`content wide-flex-col padding-none ${isScreenSmall ? 'col-100' : 'col-40'}`}>
           <div className={`component-view component-code border-button-top`}>
-            <Button className={`toggle-btn border-radius-none${tab === 'html' ? ' selected' : ''}`} onPress={() => setTab('html')}>
+            <Button className={`toggle-btn border-radius-none${tab === 'html' ? ' selected' : ''}`} onPress={() => changeTab('html')}>
               <Text font="display" weight="bold" size="xs">
                 HTML
               </Text>
             </Button>
-            <Button className={`toggle-btn border-radius-none${tab === 'css' ? ' selected' : ''}`} onPress={() => setTab('css')}>
+            <Button className={`toggle-btn border-radius-none${tab === 'css' ? ' selected' : ''}`} onPress={() => changeTab('css')}>
               <Text font="display" weight="bold" size="xs">
                 CSS
               </Text>
             </Button>
-            <Button className={`toggle-btn border-radius-none${tab === 'js' ? ' selected' : ''}`} onPress={() => setTab('js')}>
+            <Button
+              className={`toggle-btn border-radius-none${tab === 'js' ? ' selected' : ''}`}
+              onPress={() => {
+                changeTab('js')
+              }}
+            >
               <Text font="display" weight="bold" size="xs">
                 JS
               </Text>
@@ -57,7 +103,7 @@ const ComponentView = (props: ComponentViewProps) => {
           </div>
           <div className={`component-view component-code`}>
             <TextField className="component-code">
-              <TextArea />
+              <TextArea value={curTabText} onChange={e => updateText(e.target.value)} />
             </TextField>
             <div className="button-row align-right">
               <Button className="border-radius-50 button-blank">
